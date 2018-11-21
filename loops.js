0905c8a71e85bfd7bfe07162ced9131653700084
Janette Wallace <jmariewallace2@yahoo.com>
2018-11-21 17:41:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,5 +18,12 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
-  
-}
+  var i = 0
+  function incrementVariable() {
+    i = i + 1
+  }
+  do {
+    incrementVariable()
+  }
+  while array.length > 0 && incrementVariable()
+  return array
